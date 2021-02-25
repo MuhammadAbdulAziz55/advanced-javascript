c6e203cf4c7131284192660f75045052fe09e46b
MuhammadAbdulAziz55 <abdulazizfrince5@gmail.com>
2021-02-25 15:07:17 +0600

    double vs triple understand

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,53 @@
+// double equal(==)
+// ex-1
+const first = 2;
+const second = 3;
+if(first == second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+// ex-2
+const first = 2;
+const second = 2;
+if(first == second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+// ex-3
+const first = 2;
+const second = "2";
+if(first == second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+// triple equal(===)
+// ex-4
+const first = 2;
+const second = 2;
+if(first === second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+// ex-5
+const first = 2;
+const second = "2";
+if(first === second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+
